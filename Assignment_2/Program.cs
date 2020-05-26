36bcd90e4565981576ba264680278dcf034dd569
Kuan <kche440@aucklanduni.ac.nz>
2020-05-26 21:58:43 +1200

    Task 1 completed properly, however still need relevant constructors which we should ask about...

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -11,46 +11,68 @@ namespace Assignment_2
         //}
 
         //Constructors
-        public FiniteStateTable() //Default Finite State Table
+        private FiniteStateTable() //Default Finite State Table
         {
+            FST = null;
             Console.WriteLine("Default Constructor!");
-            
+        }
+
+        private FiniteStateTable(int[,] fst) //User defined size Finite State Table
+        {
+            FST = fst;
+            Console.WriteLine("User has defined a Finite State Table of total size {0}!", fst.Length);
         }
         
         //Variables
         public int[,] FST; //2D Array
 
+        // maybe implement an indexer for FST?
         struct cell_FST
         {
             //Variables for the struct
-            public int nextState;
-            public string action;
+            private static int _nextState { get; set; }
+            private static string _action { get; set; }
 
-            //Methods for the struct
-            public void setnextState(int d_state)
+            //Getter and setter methods
+            public static int nextState 
             {
-                this.nextState = d_state;
+                get { return _nextState; }
+                set { _nextState = value;  }
             }
-            public void setAction(string d_action)
+            public static string action
             {
-                this.action = d_action;
+                get { return _action; }
+                set { _action = value;  }
             }
-            public int getnextState()
-            {
-                return nextState;
-            }
-            public string getAction()
-            {
-                return action;
-            }
+            
+            //Constructor
         }
         
         //Methods
+        public void setnextState(int d_state)
+        {
+            cell_FST.nextState = d_state;
+        }
+        public void setAction(string d_action)
+        {
+            cell_FST.action = d_action;
+        }
+        public int getnextState()
+        {
+            return cell_FST.nextState;
+        }
+        public string getAction()
+        {
+            return cell_FST.action;
+        }
 
         static void Main(string[] args) // entry point 
         {
             // shit goes here
-            cell_FST fish = new cell_FST();
+            var amn = new int[5 , 5];
+            FiniteStateTable fish = new FiniteStateTable(amn);
+            fish.setAction("peepee");
+            Console.WriteLine(fish.getAction());
         }
 
     }
