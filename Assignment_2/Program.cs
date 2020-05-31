89caf638f76b5f40ac77be11c2c5d5484cc80456
Kuan <kche440@aucklanduni.ac.nz>
2020-05-31 23:09:35 +1200

    Cell_FST

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -18,7 +18,7 @@ namespace Assignment_2
             Console.WriteLine("Default Constructor!");
         }
 
-        private FiniteStateTable(int[,] fst) //User defined size Finite State Table
+        private FiniteStateTable(cell_FST[,] fst) //User defined size Finite State Table
         {
             FST = fst;
             Console.WriteLine("User has defined a Finite State Table of total size of {0} by {1}!", fst.GetLength(0),
@@ -26,66 +26,74 @@ namespace Assignment_2
         }
         
         //Variables
-        public int[,] FST; //2D Array
+        private cell_FST[,] FST; //2D Array
 
         //S0                        S1                S2
         //S1 -> ActionX/Y           S2 -> ActionX/Z   S0 -> ActionW
         // S0 -> do_nothing         S0 -> ActionW     S1 -> ActionX/Y
         // Default? -> do_nothing   S1 -> do_nothing  S2 -> do_nothing    
 
-        private const string FNAME = @"C:\Users\fifac\OneDrive\Desktop\Mechatronics Third Year\313\"; //Reuben
-        //private const string FNAME = @"C:\Users\fifac\OneDrive\Desktop\Mechatronics Third Year\313\"; //KUAN
-
-
+        // private const string FNAME = @"C:\Users\fifac\OneDrive\Desktop\Mechatronics Third Year\313\"; //Reuben
+        private const string FNAME = @"C:\Users\Kuanc\Desktop\Fourth Year\MECHENG313\Assignment 2\"; //KUAN
+        
+        
         // maybe implement an indexer for FST?
         struct cell_FST
         {
             //Variables for the struct
-            private static int _nextState { get; set; }
-            private static string _action { get; set; }
+            private int nextState;
+            private string action;
 
             //Getter and setter methods
-            public static int nextState 
+            // public static int nextState 
+            // {
+            //     get { return _nextState; }
+            //     set { _nextState = value;  }
+            // }
+            // public static string action
+            // {
+            //     get { return _action; }
+            //     set { _action = value;  }
+            // }
+            public void setnextState(int d_state)
             {
-                get { return _nextState; }
-                set { _nextState = value;  }
+                this.nextState = d_state;
             }
-            public static string action
+            public void setAction(string d_action)
             {
-                get { return _action; }
-                set { _action = value;  }
+                this.action = d_action;
             }
-            
-            //Constructor
+            public int getnextState()
+            {
+                return this.nextState;
+            }
+            public string getAction()
+            {
+                return this.action;
+            }
         }
         
         //Methods
-        public void setnextState(int d_state)
-        {
-            cell_FST.nextState = d_state;
-        }
-        public void setAction(string d_action)
-        {
-            cell_FST.action = d_action;
-        }
-        public int getnextState()
-        {
-            return cell_FST.nextState;
-        }
-        public string getAction()
-        {
-            return cell_FST.action;
-        }
+        
 
         static void Main(string[] args) // entry point 
         {
+            
             // shit goes here
-            //var amn = new int[5, 5];
-            //FiniteStateTable fish = new FiniteStateTable(amn);
-            //fish.setAction("peepee");
+            Action[,] Penis = new Action[5, 5];
+            var fish = new FiniteStateTable(Penis);
+            cell_FST S0 = new cell_FST();
+            S0.setAction("peepee");
+            S0.setnextState(1);
+            fish.FST[0, 0] = S0;
+            
+            
+            
+  
+            fish.FST = 
             //Console.WriteLine(fish.getAction());
 
-            //code to read strings input from user we will recieve a,b,c and use compare
+            //code to read strings input from user we will receive a,b,c and use compare
             //we will also need invalid key handling as well as 'q' for quit handling
             //Console.WriteLine("Please enter some bullshit");
             //string age = Console.ReadLine();
@@ -97,14 +105,13 @@ namespace Assignment_2
             //Console.WriteLine("Print action associated with transistion");
             //Console.WriteLine("Now in state S1");
 
-            string run = "";
+            /*string run = "";
             string log = String.Format("{0,0} {1,20} {2,15} {3,15}\n", "TimeStamp", "Trigger", "Event", "Action");
 
             while (run != "q")
             {
                 Console.WriteLine("trigger");
                 run = Console.ReadLine();
-                Console.WriteLine(run);
 
                 log += String.Format("{0,0} {1,10} {2,15} {3,10} {4,-15}\n", DateTime.Now.ToString("yyyy MM dd HH mm ss"), run, "Event", "", "Action");
 
@@ -117,7 +124,7 @@ namespace Assignment_2
 
             string file_name = FNAME + Name + ".txt";
 
-            System.IO.File.AppendAllText(file_name, (log) + "\n");
+            System.IO.File.AppendAllText(file_name, (log) + "\n");*/
 
             
             
