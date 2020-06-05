6e83fdee2b68de7be158ea3b5190425ff50cdb7a
Reuben <robr763@aucklanduni.ac.nz>
2020-06-05 15:44:59 +1200

    Super Sexy Code

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assignment_2
 {
@@ -11,12 +12,7 @@ namespace Assignment_2
         //}
 
         //Constructors
-        private FiniteStateTable() //Default Finite State Table
-        {
-            FST = null;
-            Console.WriteLine("Default Constructor!");
-        }
-
+      
         private FiniteStateTable(cell_FST[,] fst) //User defined size Finite State Table
         {
             FST = fst;
@@ -29,6 +25,9 @@ namespace Assignment_2
 
         private const string FNAME = @"C:\Users\fifac\OneDrive\Desktop\Mechatronics Third Year\313\"; //Reuben
         //private const string FNAME = @"C:\Users\Kuanc\Desktop\Fourth Year\MECHENG313\Assignment 2\"; //KUAN
+
+      //  protected Action ActionX(void)      
+
         
         
         // maybe implement an indexer for FST?
@@ -55,11 +54,9 @@ namespace Assignment_2
             {
                 return this.action;
             }
-        }
-        
-        //Methods
-      
+        }   
 
+        //Methods //needs be its on classssss
         static void Main(string[] args) // entry point 
         {
             cell_FST[,] FSM = new cell_FST[3, 3];
@@ -69,6 +66,7 @@ namespace Assignment_2
             //S1 -> ActionX/Y           S0 -> ActionW        S0 -> ActionW     //a
             //S0 -> do_nothing          S2 -> ActionX/Z      S2 -> do_nothing  //b
             //S0 -> do_nothing          S1 -> do_nothing     S1 -> ActionX/Y   //c 
+
             fish.FST[0,0].setAction("ActionX/Y"); // STATE 0 Transition to state 1
             fish.FST[0,0].setnextState(1);
             fish.FST[0,1].setAction("ActionW"); // STATE 1 Transition to state 0
@@ -88,91 +86,55 @@ namespace Assignment_2
             fish.FST[2,2].setAction("ActionX/Y"); // STATE 2 Transition to state 1
             fish.FST[2,2].setnextState(1);
             
-            var current_state = 0;
-            string action = "";
-            int x = 0;
-            int y = 0;
-            int last_x = -1;
+            //Variables
+            string actionPerform = "";                                
+            string timestamp = "";
+
+            int x = -1; 
+            int current_state = 0;
+
+            ConsoleKey key_in = ConsoleKey.Delete; 
             
-            //current.state = state 1
+            string log = "TimeStamp \t\t Event \t\t Action \n"; //initialise log file
 
-            //code to read strings input from user we will receive a,b,c and use compare
-
-                 
-            string key_in = "";
-            string log = String.Format("{0,0} {1,20} {2,15} {3,15}\n", "TimeStamp", "Trigger", "Event", "Action");
-
-            while (key_in != "q")
-            {
-                Console.WriteLine("trigger");
-                key_in = Console.ReadLine();
-
-                switch (current_state)
+            //Runnning Machine
+            while (key_in != ConsoleKey.Q)
+            {             
+                key_in = Console.ReadKey(true).Key;
+                
+                    switch (key_in)
                 {
-                    case 0:
-                        if (key_in == "a")
-                        {                            
-                            x = 0;
-                            y = 0;
-                        }                        
+                    case ConsoleKey.A:                                                
+                        x = 0;                               
                         break;
-
-                    case 1:
-                        if (key_in == "a")
-                        {                            
-                            x = 0;
-                            y = 1;
-                        }
-                        else if (key_in == "b")
-                        {                            
-                            x = 1;
-                            y = 1;                            
-                        }
-                        
+                    case ConsoleKey.B:
+                        x = 1;
                         break;
-
-                    case 2:
-                        if (key_in == "a")
-                        {                            
-                            x = 0;
-                            y = 2;                            
-                        }
-                        else if (key_in == "c")
-                        {                            
-                            x = 2;
-                            y = 2;
-                        }                        
+                    case ConsoleKey.C:
+                        x = 2;                     
                         break;
                 }
-                if (last_x == x) 
+                
+                if (key_in == ConsoleKey.A || key_in == ConsoleKey.B || key_in == ConsoleKey.C)
                 {
-                    action = "Do_Nothing";
-                }
-                else
-                {
-                    action = (fish.FST[x, y].getAction());
-                    current_state = fish.FST[x, y].getnextState();
-                }
+                    actionPerform = (fish.FST[x, current_state].getAction());
+                    current_state = fish.FST[x, current_state].getnextState();
 
-                last_x = x;                
+                    Console.WriteLine(current_state);
+                    Console.WriteLine(actionPerform);
 
-                Console.WriteLine(action);
-
-                log += String.Format("{0,0} {1,10} {2,15} {3,10} {4,-15}\n", DateTime.Now.ToString("yyyy MM dd HH mm ss"), key_in, "Event", "", action );
-
+                    timestamp = DateTime.Now.ToString("yyyy MM dd HH mm ss");
+                    log += timestamp + "\t" + " " + key_in + "\t\t" + " " + actionPerform + "\n";
+                }                                  
             }
 
-            //Logging the actions and output to files should happen somewhere here
-
+            //Creating File
             Console.WriteLine("File Name:");
             string Name = Console.ReadLine();
 
             string file_name = FNAME + Name + ".txt";
 
-            System.IO.File.AppendAllText(file_name, (log) + "\n");         
-            
+            System.IO.File.AppendAllText(file_name, (log) + "\n");                 
         }
-
-    }
-    
+    }    
 }
